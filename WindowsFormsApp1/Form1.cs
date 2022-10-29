04bf75fe966f7af63a9237c7e6942bafa452158e
p36828 <p36828@206-19>
2022-10-29 14:27:35 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int add = int.Parse(textBox1.Text);
+            int sum = int.Parse(textBox2.Text);
+            Account acc = new Account(add);
+            acc.RegisterHandler(PrintSimpleMessage);
+            acc.Take(sum);
+            void PrintSimpleMessage(string message) => MessageBox.Show(message);
+        }
+        public class Account
+        {
+            public delegate void AccountHandler(string message);
+            int sum;
+            // Создаем переменную делегата
+            AccountHandler taken;
+            public Account(int sum) => this.sum = sum;
+            // Регистрируем делегат
+            public void RegisterHandler(AccountHandler del)
+            {
+                taken = del;
+            }
+            public void Add(int sum) => this.sum += sum;
+            public void Take(int sum)
+            {
+                if (this.sum >= sum)
+                {
+                    this.sum -= sum;
+                    // вызываем делегат, передавая ему сообщение
+                    taken?.Invoke($"Со счета списано {sum} у.е.");
+                }
+                else
+                {
+                    taken?.Invoke($"Недостаточно средств. Баланс: {this.sum} у.е.");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int add = int.Parse(textBox2.Text);
+            Account acc = new Account(add);
+            acc.Add(add);
+            acc.RegisterHandler(PrintSimpleMessage);
+            void PrintSimpleMessage(string message) => MessageBox.Show(message);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
